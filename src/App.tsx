94c2451c1e7d492d8b98adf24af1d71b95842da8
Jakub Janicki <jjanicki08@gmail.com>
2026-09-17 23:56:17 +0200

    feat(initial config): initial config of the project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Test from '@components/test'
+
+function App() {
+    return (
+        <>
+            <p className="text-4xl font-bold underline text-tahiti">Hello World!</p>
+        </>
+    )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: [
+            {
+                find: '@components',
+                replacement: path.resolve(import.meta.dirname, './src/components'),
+            },
+            {
+                find: '@store',
+                replacement: path.resolve(import.meta.dirname, './src/store'),
+            },
+        ],
+    },
+})
